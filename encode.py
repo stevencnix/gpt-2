7bc0dfe659e000ca74d83ed4f3fde25f595483e4
Steven Nix <stevencnix@gmail.com>
2019-11-07 23:40:13 -0500

    updated encode.py to correctly use the need models directory paramter require for get_encoder in the encoder package.

--- encode.py
+++ encode.py
@@ -4,6 +4,8 @@
 #  PYTHONPATH=src ./train --dataset /path/to/output.npz
 
 import argparse
+import os
+
 import numpy as np
 
 import encoder
@@ -13,6 +15,7 @@ parser = argparse.ArgumentParser(
     description='Pre-encode text files into tokenized training set.',
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--model_name', metavar='MODEL', type=str, default='1558M', help='Pretrained model name')
+parser.add_argument('--models_dir', metavar='PATH', type=str, default="models", help='Pretrained models dir')
 parser.add_argument('--combine', metavar='CHARS', type=int, default=50000, help='Concatenate files with <|endoftext|> separator into chunks of this minimum size')
 parser.add_argument('--encoding', type=str, default='utf-8', help='Set the encoding for reading and writing files.')
 parser.add_argument('in_text', metavar='PATH', type=str, help='Input file, directory, or glob pattern (utf-8 text).')
@@ -20,7 +23,8 @@ parser.add_argument('out_npz', metavar='OUT.npz', type=str, help='Output file pa
 
 def main():
     args = parser.parse_args()
-    enc = encoder.get_encoder(args.model_name)
+    models_dir = os.path.expanduser(os.path.expandvars(args.models_dir))
+    enc = encoder.get_encoder(args.model_name, models_dir)
     print('Reading files')
     chunks = load_dataset(enc, args.in_text, args.combine, encoding=args.encoding)
     print('Writing', args.out_npz)
